c2d515e845db0db64c97541f8fe9d1bf282cbd9e
Mehmet Demirci <oa.mehmetdmrc@gmail.com>
2022-01-12 10:38:44 +0300

    gitignore

--- compile.sh
+++ compile.sh
@@ -1,12 +1,22 @@
-# if [ -z "$2" ]
+# # if [ -z "$2" ]
+# #     then
+# #     echo "Too few arguments are provided"
+# #     exit 1
+# # fi
+# flex lexer.l
+# bison -dy --verbose parser.y
+# g++ -w -g y.tab.c lex.yy.c
+# if [ -n "$1" ]
 #     then
-#     echo "Too few arguments are provided"
-#     exit 1
+#     ./a.exe ./$1.c
 # fi
-flex lexer.l
-bison -dy --verbose parser.y
-g++ -w -g y.tab.c lex.yy.c node_base.cpp
+
+flex -o rules/lexer.cpp rules/lexer.l
+bison -d --verbose -o rules/parser.cpp rules/parser.y
+
+g++ -w -g -o MPL.exe main.cpp rules/lexer.cpp rules/parser.cpp
+
 if [ -n "$1" ]
     then
-    ./a.exe ./$1.c
+    ./MPL.exe ./$1.c
 fi
