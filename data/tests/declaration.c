8c6d6cbf16517a96c4d6560d70837a8a8d07976f
Mehmet Demirci <oa.mehmetdmrc@gmail.com>
2022-01-13 19:30:41 +0300

    test cases

---
+++ data/tests/declaration.c
@@ -0,0 +1,15 @@
+int i; //global declarations
+string si;
+bool bi;
+double di;
+
+void main(){
+    int i = 5; //local declaration
+    string si;
+    bool bi;
+    double di;
+    while(i < 10){
+        i = i+1;
+        int sv = 31;
+    }
+}
